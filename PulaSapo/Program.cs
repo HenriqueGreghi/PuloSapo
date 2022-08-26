1a3c4c6ff73e05fa343db9748e92b7af2d960c0a
Henrique Greghi <henriquevsg@gmail.com>
2022-08-26 12:11:07 -0300

    pula sapo

---
+++ PulaSapo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PulaSapo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Entre com o número pulos e de canos: ");
+            string[] valores = Console.ReadLine().Split(' ');
+            int pulos = int.Parse(valores[0]);
+            int numeroCanos = int.Parse(valores[1]);
+            bool WIN = true;
+            Console.Write("Entre com as alturas dos canos : ");
+            string[] Cano = Console.ReadLine().Split(' ');
+            int[] Canos = Array.ConvertAll(Cano, int.Parse);
+
+            for (int i = 0; i< (numeroCanos - 1); i++)
+            {
+                if ((Canos[i] - Canos[i+1]) < (- pulos))
+                {
+                    WIN = false;
+                }
+                else if ((Canos[i]- Canos[i + 1]) > (pulos))
+                {
+                    WIN = false;
+                }
+            }
+            if (WIN)
+            {
+                Console.WriteLine("YOU WIN");
+            }
+            else 
+            {
+                Console.WriteLine("GAME OVER");
+            }
+           
+        }
+    }
+}
---
+++ PulaSapo/Pulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PulaSapo
+{
+    class Pulo
+    {
+        public int Pulos { get; set; }
+        public int NumeroCanos { get; set; }
+       
+
+        public Pulo(int pulos, int numeroCanos)
+        {
+            Pulos = pulos;
+            NumeroCanos = numeroCanos;
+            
+        }
+        public string Sapo()
+        {
+            
+            if (NumeroCanos > Pulos)
+            {
+                return "YOU WIN";
+            }
+            else
+            {
+                return"GAME OVER!!!";
+            }
+        }
+    }
+}
